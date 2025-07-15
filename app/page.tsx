019b25cff0d67c781f099f4391ec33e1fd178bad
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-15 20:00:13 +0100

    setup home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,167 @@
-import Image from "next/image";
+'use client';
+
+import {useState} from 'react';
+import BtnLink from '@/components/BtnLink';
+import Button from '@/components/Button';
+import EmptyResult from '@/components/EmptyResult';
+import Input from '@/components/Input';
+import Radio from '@/components/Radio';
+import Result from '@/components/Result';
+import {calculateMortgage, MortgageDetails, MortgageResult} from '@/lib/utils';
+
+interface MortgageForm {
+    amount: string;
+    term: string;
+    rate: string;
+    type: string | 'repayment' | 'interest-only';
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const [mortgageForm, setMortgageForm] = useState<MortgageForm>({
+        amount: '',
+        term: '',
+        rate: '',
+        type: '',
+    });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const clearAll: () => void = () => {
+        setMortgageForm({
+            amount: '',
+            term: '',
+            rate: '',
+            type: '',
+        });
+    };
+
+    const [errors, setErrors] = useState<Record<string, string>>({});
+
+    const validateForm = (): boolean => {
+        const newErrors: Record<string, string> = {};
+
+        if (!mortgageForm.amount) {
+            newErrors.amount = 'This field is required';
+        }
+        if (!mortgageForm.term) {
+            newErrors.term = 'This field is required';
+        }
+        if (!mortgageForm.rate) {
+            newErrors.rate = 'This field is required';
+        }
+        if (!mortgageForm.type) {
+            newErrors.type = 'This field is required';
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const [result, setResult] = useState<MortgageResult | undefined>();
+
+    const handleFieldChange = (field: string, value: string) => {
+        setMortgageForm((prev) => ({
+            ...prev,
+            [field]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!validateForm()) {
+            return;
+        }
+
+        const data = calculateMortgage(mortgageForm as MortgageDetails);
+        setResult(data);
+    };
+
+    return (
+        <div className="bg-(--clr-white) md:m-(--space-500) md:rounded-3xl  overflow-hidden">
+            <div className="flex flex-col lg:flex-row md:w-[43rem] lg:w-[63rem]">
+                {/* Calculator */}
+                <form
+                    onSubmit={handleSubmit}
+                    className="calculator-wrapper bg-(--clr-white) flex flex-col gap-y-(--space-300) px-(--space-300) py-(--space-400) md:p-(--space-500) md:gap-y-(--space-500) lg:flex-1">
+                    <div className="header flex flex-col justify-start gap-y-(--space-100)  md:justify-between md:flex-row">
+                        <h1 className="text-(--clr-slate-900) text-(length:--fs-24) font-bold leading-(--lh-125)">
+                            Mortgage Calculator
+                        </h1>
+                        <BtnLink clearAll={clearAll} />
+                    </div>
+
+                    {/* Inputs */}
+                    <div className="flex flex-col gap-y-(--space-300)">
+                        <Input
+                            prefix
+                            prefixLabel="£"
+                            label="Mortgage Amount"
+                            name="amount"
+                            value={mortgageForm.amount}
+                            handleChange={handleFieldChange}
+                            error={errors.amount}
+                        />
+
+                        <div className="flex flex-col gap-y-(--space-300) md:flex-row md:justify-between md:gap-x-[1.5rem]">
+                            <Input
+                                suffix
+                                label="Mortgage Term"
+                                name="term"
+                                suffixLabel="years"
+                                value={mortgageForm.term}
+                                handleChange={handleFieldChange}
+                                error={errors.term}
+                            />
+                            <Input
+                                suffix
+                                label="Interest Rate"
+                                suffixLabel="%"
+                                name="rate"
+                                value={mortgageForm.rate}
+                                handleChange={handleFieldChange}
+                                error={errors.rate}
+                            />
+                        </div>
+
+                        {/* Radio Inputs */}
+                        <div className="flex flex-col gap-y-(--space-150)">
+                            <p className="text-(--clr-slate-700) text-(length:--fs-16) font-medium leading-(--lh-500)">
+                                Mortgage Type
+                            </p>
+                            <Radio
+                                label="Repayment"
+                                name="morgage-type"
+                                value="repayment"
+                                checked={mortgageForm.type === 'repayment'}
+                                onChange={(value) =>
+                                    handleFieldChange('type', value)
+                                }
+                            />
+                            <Radio
+                                label="Interest Only"
+                                name="morgage-type"
+                                value="interest-only"
+                                checked={mortgageForm.type === 'interest-only'}
+                                onChange={(value) =>
+                                    handleFieldChange('type', value)
+                                }
+                            />
+                            {errors.type && (
+                                <p className="error-text text-(--clr-red) text-(length:--fs-14) font-medium leading-(--lh-150)">
+                                    {errors.type}
+                                </p>
+                            )}
+                        </div>
+                    </div>
+
+                    {/* Button */}
+                    <Button label="Calculate Repayments" type="submit" />
+                </form>
+
+                {/* Result  */}
+                <div className="result flex items-center justify-center bg-(--clr-slate-900) py-(--space-400) px-(--space-300) md:p-(--space-500) lg:flex-1 lg:rounded-bl-[5rem]">
+                    {result ? <Result result={result} /> : <EmptyResult />}
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
